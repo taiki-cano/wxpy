f82a3cad578c54577893da7058e7a7498fbde4f9
taiki <iwamoto.taiki@gmail.com>
2020-08-25 10:53:01 +0900

    add Mainpanel

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+# -*- coding : <utf-8> -*-
+# main.py
+
 import wx
+
+from pubsub import pub
 
 
 class MainFrame(wx.Frame):
     def __init__(self):
         super().__init__(None, title='wxpy')
         self.Show()
+
+
+class MainPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
 
 
 def main():

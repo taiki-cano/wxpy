f5e4273439dd48dd7e74b2b72ac938ce09337c87
taiki <iwamoto.taiki@gmail.com>
2020-08-26 17:10:58 +0900

    MVC Model

--- model.py
+++ model.py
@@ -1,13 +1,38 @@
 # -*- coding : <utf-8> -*-
 # model.py
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import Table, Column, create_engine
+from sqlalchemy import Integer, ForeignKey, String, Unicode
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import backref, relation
 
 engine = create_engine('sqlite:///books.db', echo=True)
 Base = declarative_base()
+metadata = Base.metadata
+
+
+class OlvBook(object):
+    """
+    * Book model for ObjectListView
+    """
+    def __init__(self, id, title, author, isbn, publisher, last_name, first_name):
+        self.id = id  # unique row id from database
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.publisher = publisher
+        self.last_name = last_name
+        self.first_name = first_name
+
+
+class Person(Base):
+    __tablename__ = "people"
+    id = Column(Integer, primary_key=True)
+    first_name = Column("ファーストネーム", String(50))
+    last_name = Column("ラストネーム", String(50))
+
+    def __repr__(self):
+        return "<Person: %s %s>" % (self.first_name, self.last_name)
 
 
 class Book(Base):
@@ -16,52 +41,8 @@ class Book(Base):
     """
     __tablename__ = "books"
     id = Column(Integer, primary_key=True)
-    title = Column(String)
-    author = Column(String)
-
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
-
-
-class Character(Base):
-    __tablename__ = "characters"
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    book_id = Column(ForeignKey("books.id"))
-    book = relationship("Book", backref=backref("characters", order_by=id))
-
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-
-    @property
-    def fullname(self):
-        """
-        * フルネームを返す
-        """
-        return "%s %s" % (self.first_name, self.last_name)
-
-    def __repr__(self):
-        """
-        * 文字列の公式表現をオーバーライド
-        """
-        return "<Character(%s)>" % self.fullname
-
-
-if __name__ == "__main__":
-    # Create the table
-    Base.metadata.create_all(engine)
-    # Create a session object (データベースにデータを入力出来るようにする)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Add data to the tables
-    new_char = Character("直樹", "半沢")
-    new_char.book = Book("オレたちバブル入行組", "池井戸潤")
-    session.add(new_char)
-    new_char = Character("アメリカ", "フェレーラ")
-    new_char.book = Book("アグリー・ベティ", "シルヴィオ・ホルタ")
-    session.add(new_char)
-    session.commit()
+    author_id = Column(Integer, ForeignKey("people.id"))
+    title = Column("title", Unicode)
+    isbn = Column("isbn", Unicode)
+    publisher = Column("publisher", Unicode)
+    person = relation("Person", backref="books", cascade_backrefs=False)

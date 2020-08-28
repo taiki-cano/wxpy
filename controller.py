68f26f37feae10c0343744e9952d5d16b41024ef
taiki <iwamoto.taiki@gmail.com>
2020-08-28 17:50:21 +0900

    MVC Controller

--- controller.py
+++ controller.py
@@ -24,3 +24,85 @@ def add_recourd(session, data):
 
     session.add(book)
     session.commit()
+
+
+def connect_to_database():
+    """
+    * Connect to the SQLite database and return a Session object
+    """
+    engine = create_engine("sqlite:///books.db", echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+def convert_results(results):
+    """
+    * Convert results to OlvBook objects
+    """
+    books = []
+    for record in results:
+        author = "%s %s" % (record.person.first_name, record.person.last_name)
+        book = OlvBook(record.id, record.title, author, record.isbn,
+                       record.publisher, record.person.last_name, record.person.first_name)
+        books.append(book)
+    return books
+
+
+def delete_record(session, id_num):
+    """
+    * Delete a record from the database
+    """
+    record = session.query(Book).filter_by(id=id_num).one()
+    session.delete(record)
+    session.commit()
+
+
+def edit_record(session, id_num, row):
+    """
+    * Edit a record
+    """
+    record = session.query(Book).filter_by(id=id_num).one()
+    record.title = row["title"]
+    record.person.first_name = row["first_name"]
+    record.person.last_name = row["last_name"]
+    record.isbn = row["isbn"]
+    record.publisher = row["publisher"]
+    session.add(record)
+    session.commit()
+
+
+def get_all_records(session):
+    """
+    * Get all records and return them
+    """
+    result = session.query(Book).all()
+    books = convert_results(result)
+    return books
+
+
+def search_records(session, filter_choice, keyword):
+    """
+    * Searches the detabase based on the fillter chosen and the keyword
+    * given by the user
+    """
+    if filter_choice == "Author":
+        qry = session.query(Person)
+        result = qry.filter(Person.first_name.contains('%s' % keyword)).all()
+        records = []
+        for record in result:
+            for book in record.books:
+                records.append(book)
+        result = records
+    elif filter_choice == "Title":
+        qry = session.query(Book)
+        result = qry.filter(Book.title.contains('%s' % keyword)).all()
+    elif filter_choice == "ISBN":
+        qry = session.query(Book)
+        result = qry.filter(Book.isbn.contains('%s' % keyword)).all()
+    else:
+        qry = session.query(Book)
+        result = qry.filter(Book.publisher.contains('%s' % keyword)).all()
+    books = convert_results(result)
+
+    return books

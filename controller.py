0d2fe5d6a40ca0a7a53af8e0ad0f86c3be057d85
taiki <iwamoto.taiki@gmail.com>
2020-08-31 13:14:36 +0900

    MVC Controller

--- controller.py
+++ controller.py
@@ -6,13 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def add_recourd(session, data):
-    """
-    * Data should be a dictionary of two dictionaries in the following format:
-    *    {"author":{"first_name":"John", "last_name":"Doe"},
-    *   "book":{"title":"Some book", "isbn":"1234567890",
-    *    "publisher":"Packt"}}
-    """
+def add_record(session, data):
     book = Book()
     book.title = data["book"]["title"]
     book.isbn = data["book"]["isbn"]
@@ -20,16 +14,17 @@ def add_recourd(session, data):
     author = Person()
     author.first_name = data["author"]["first_name"]
     author.last_name = data["author"]["last_name"]
-    book.publisher = author
+    book.person = author
 
+    # try:
     session.add(book)
     session.commit()
+    # except Exception as e:
+    #     print("add Error : %s" % e )
+    #     session.rollback()
 
 
 def connect_to_database():
-    """
-    * Connect to the SQLite database and return a Session object
-    """
     engine = create_engine("sqlite:///books.db", echo=True)
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -37,9 +32,6 @@ def connect_to_database():
 
 
 def convert_results(results):
-    """
-    * Convert results to OlvBook objects
-    """
     books = []
     for record in results:
         author = "%s %s" % (record.person.first_name, record.person.last_name)
@@ -50,18 +42,12 @@ def convert_results(results):
 
 
 def delete_record(session, id_num):
-    """
-    * Delete a record from the database
-    """
     record = session.query(Book).filter_by(id=id_num).one()
     session.delete(record)
     session.commit()
 
 
 def edit_record(session, id_num, row):
-    """
-    * Edit a record
-    """
     record = session.query(Book).filter_by(id=id_num).one()
     record.title = row["title"]
     record.person.first_name = row["first_name"]
@@ -73,19 +59,12 @@ def edit_record(session, id_num, row):
 
 
 def get_all_records(session):
-    """
-    * Get all records and return them
-    """
     result = session.query(Book).all()
     books = convert_results(result)
     return books
 
 
 def search_records(session, filter_choice, keyword):
-    """
-    * Searches the detabase based on the fillter chosen and the keyword
-    * given by the user
-    """
     if filter_choice == "Author":
         qry = session.query(Person)
         result = qry.filter(Person.first_name.contains('%s' % keyword)).all()
@@ -106,3 +85,7 @@ def search_records(session, filter_choice, keyword):
     books = convert_results(result)
 
     return books
+
+
+def setup_database():
+    metadata.create_all()

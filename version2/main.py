35d3c0a1425703220956b626a38631d17be42d7f
taiki <iwamoto.taiki@gmail.com>
2020-09-03 21:41:36 +0900

    BoxSizer

--- version2/main.py
+++ version2/main.py
@@ -7,45 +7,51 @@ import wx
 class MainFrame(wx.Frame):
     def __init__(self):
         super().__init__(None, title="パネル配置", size=(800, 600))
-        self.SetBackgroundColour('#000000')
-        self.InitializeComponents()
-        self.Show()
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        mainPanel = wx.Panel(self, -1, style=wx.BORDER_SIMPLE)
+        main_sizer.Add(mainPanel, -1, wx.EXPAND)
 
-    def InitializeComponents(self):
-        self.mainPanel = wx.Panel(self, -1, size=(600, 400), style=wx.BORDER_SIMPLE)
-        self.mainPanel.SetBackgroundColour('#FFFFFF')
-        maindisplay = MainDisplay(self)
-        subdisplay = SubDisplay(self)
-        toolbox = ToolBox(self)
+        main_display = wx.Panel(self, wx.ID_ANY, style=wx.BORDER_SIMPLE)
+        main_display.SetBackgroundColour('#FF0000')
+        sub_display = wx.Panel(self, wx.ID_ANY, style=wx.BORDER_SIMPLE)
+        sub_display.SetBackgroundColour('#00FF00')
+        tool_box = wx.Panel(self, wx.ID_ANY, style=wx.BORDER_SIMPLE)
+        tool_box.SetBackgroundColour('#0000FF')
 
-        sizer = wx.GridBagSizer()
-        sizer.Add(maindisplay, (0, 0), (1, 2), flag=wx.EXPAND)
-        sizer.Add(subdisplay, (0, 2), (1, 1), flag=wx.EXPAND)
-        sizer.Add(toolbox, (1, 0), (1, 2), flag=wx.EXPAND)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        upper_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        upper_sizer.Add(main_display, wx.ID_ANY, wx.EXPAND)
+        upper_sizer.Add(tool_box, wx.ID_ANY, wx.EXPAND)
+        sizer.Add(upper_sizer, wx.ID_ANY, wx.EXPAND)
+        sizer.Add(sub_display, wx.ID_ANY, wx.EXPAND)
 
-        self.mainPanel.SetSizer(sizer)
+        mainPanel.SetSizer(sizer)
+        self.SetSizer(main_sizer)
 
 
 class MainDisplay(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
-        displaypanel = wx.Panel(parent.mainPanel, -1, pos=(0, 0), size=(250, 250))
+        displaypanel = wx.Panel(parent.mainPanel)
         displaypanel.SetBackgroundColour('#FF0000')
 
 
 class SubDisplay(wx.Panel):
     def __init__(self, parent):
-        super().__init__(parent.mainPanel, -1, pos=(250, 0), size=(150, 150))
+        super().__init__(parent)
+        subdisplaypanel = wx.Panel(parent.mainPanel)
         self.SetBackgroundColour('#00FF00')
 
 
 class ToolBox(wx.Panel):
     def __init__(self, parent):
-        super().__init__(parent.mainPanel, -1, pos=(0, 250), size=(50, 50))
+        super().__init__(parent)
+        toolboxpanel = wx.Panel(parent.mainPanel)
         self.SetBackgroundColour('#0000FF')
 
 
 if __name__ == "__main__":
     app = wx.App(redirect=False)
     frame = MainFrame()
+    frame.Show()
     app.MainLoop()

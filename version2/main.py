815f202292540160be000112cef7a1234d041cb8
taiki <iwamoto.taiki@gmail.com>
2020-09-06 16:21:45 +0900

    ScrolledWindow

--- version2/main.py
+++ version2/main.py
@@ -10,22 +10,25 @@ class MainFrame(wx.Frame):
         self.InitializeComponents()
 
     def InitializeComponents(self):
-        top_sp = wx.SplitterWindow(self, -1, style=wx.SP_LIVE_UPDATE | wx.SP_3DSASH)
-        sp = wx.SplitterWindow(top_sp, -1, style=wx.SP_LIVE_UPDATE | wx.SP_3DSASH)
-        self.main_display = wx.Panel(sp, 1)
-        self.tool_box = wx.Panel(sp, 1)
-        self.sub_display = wx.ScrolledWindow(top_sp, -1) # wx.Panel(top_sp, 1, style=wx.HSCROLL)
+        top_sp = wx.SplitterWindow(
+            self, -1, style=wx.SP_LIVE_UPDATE | wx.SP_3DSASH)
+        sp = wx.SplitterWindow(
+            top_sp, -1, style=wx.SP_LIVE_UPDATE | wx.SP_3DSASH)
+        self.main_display = wx.ScrolledWindow(sp, -1)
+        self.sub_display = wx.ScrolledWindow(sp, -1)
+        self.tool_box = wx.Panel(top_sp, 1)
 
         self.Create_ToolBox(self, title='Tool_Box', target=self.tool_box)
         self.Create_MainDisplay(self, title='Main_Display', target=self.main_display)
         self.Create_SubDisplay(self, title='Sub_Display', target=self.sub_display)
+        self.main_display.SetScrollRate(10, 10)
         self.sub_display.SetScrollRate(10, 10)
 
-        sp.SetSashGravity(0.8)
-        top_sp.SetSashGravity(0.7)
-        sp.SplitVertically(self.main_display, self.tool_box)
+        sp.SetSashGravity(0.7)
+        top_sp.SetSashGravity(0.8)
+        sp.SplitHorizontally(self.main_display, self.sub_display)
         sp.SetMinimumPaneSize(1)
-        top_sp.SplitHorizontally(sp, self.sub_display)
+        top_sp.SplitVertically(sp, self.tool_box)
         top_sp.SetMinimumPaneSize(1)
 
     def Create_ToolBox(self, parent, title, target):
@@ -38,15 +41,16 @@ class MainFrame(wx.Frame):
 
     def Create_MainDisplay(self, parent, title, target):
         maindisp_sizer = wx.StaticBoxSizer(wx.VERTICAL, target, title)
-        maindisp = wx.Panel(target, -1, style=wx.SUNKEN_BORDER)
-        maindisp.SetBackgroundColour('#00FF56')
+        maindisp = wx.Panel(target, -1, style=wx.SUNKEN_BORDER, size=(800, 600))
+        maindisp.SetBackgroundColour('#009C9E')
         maindisp_sizer.Add(maindisp, 1, wx.EXPAND)
         target.SetSizer(maindisp_sizer)
 
     def Create_SubDisplay(self, parent, title, target):
         subdisp_sizer = wx.StaticBoxSizer(wx.HORIZONTAL, target, title)
         for _ in range(15):
-            subdisp = wx.Panel(target, -1, style=wx.SUNKEN_BORDER, size=(200, 100))
+            subdisp = wx.Panel(
+                target, -1, style=wx.SUNKEN_BORDER, size=(200, 100))
             subdisp.SetBackgroundColour('#FF7896')
             subdisp_sizer.Add(subdisp, 0, wx.ALIGN_CENTER)
         target.SetSizer(subdisp_sizer)

71378b7c1c08b2a1f3e4f36570249f56ba010fcc
taiki <iwamoto.taiki@gmail.com>
2020-09-06 10:43:18 +0900

    SplitterWindow

--- version2/main.py
+++ version2/main.py
@@ -12,16 +12,29 @@ class MainFrame(wx.Frame):
     def InitializeComponents(self):
         top_sp = wx.SplitterWindow(self, -1, style=wx.SP_LIVE_UPDATE | wx.SP_3DSASH)
         sp = wx.SplitterWindow(top_sp, -1, style=wx.SP_LIVE_UPDATE | wx.SP_3DSASH)
-        main_display = wx.Panel(sp, style=wx.SUNKEN_BORDER)
-        main_display.SetBackgroundColour('#FF0000')
-        sub_display = wx.Panel(sp, style=wx.SUNKEN_BORDER)
-        sub_display.SetBackgroundColour('#00FF00')
-        sp.SetSashGravity(0.8)
-        sp.SplitVertically(main_display, sub_display)
-        tool_box = wx.Panel(top_sp, wx.ID_ANY, style=wx.BORDER_SIMPLE)
-        tool_box.SetBackgroundColour('#0000FF')
+        self.main_display = wx.Panel(sp, wx.ID_ANY, style=wx.SUNKEN_BORDER)
+        self.tool_box = wx.Panel(sp, wx.ID_ANY, style=wx.SUNKEN_BORDER | wx.ALIGN_CENTER)
+        self.sub_display = wx.Panel(top_sp, wx.ID_ANY, style=wx.SUNKEN_BORDER)
+
+        ctb = self.Create_SBS(self, title='Tool_Box', target=self.tool_box)
+        # self.tool_box.SetSizer(ctb)
+
+        sp.SetSashGravity(0.9)
         top_sp.SetSashGravity(0.7)
-        top_sp.SplitHorizontally(sp, tool_box)
+        sp.SplitVertically(self.main_display, self.tool_box)
+        top_sp.SplitHorizontally(sp, self.sub_display)
+
+    def Create_SBS(self, parent, title, target):
+        # btn_sizer = wx.StaticBoxSizer(wx.VERTICAL, parent.tool_box, 'Tool Box')
+        # btn1 = wx.Button(parent.tool_box, -1, 'ぼたん1')
+        # btn2 = wx.Button(parent.tool_box, -1, 'ボタン2')
+        btn_sizer = wx.StaticBoxSizer(wx.VERTICAL, target, title)
+        btn1 = wx.Button(target, -1, 'ぼたん1')
+        btn2 = wx.Button(target, -1, 'ボタン2')
+        btn_sizer.Add(btn1)
+        btn_sizer.Add(btn2)
+        target.SetSizer(btn_sizer)
+        # return btn_sizer
 
 
 if __name__ == "__main__":
